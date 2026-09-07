b809ab2ed4d4f0d8f3aee9b633d7cda81ec595db
Fabiano Alves Santos <fabiano.alves.dev@gmail.com>
2026-09-07 10:00:35 -0300

    fix: atualiza SafeAreaView para safe area context

--- portifolio-mobile/src/app/index.tsx
+++ portifolio-mobile/src/app/index.tsx
@@ -1,5 +1,6 @@
 import { useRef } from 'react';
-import { SafeAreaView, ScrollView, StyleSheet, View } from 'react-native';
+import { ScrollView, StyleSheet, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 import Cabecalho from '../componentes/Cabecalho';
 import Contato from '../componentes/Contato';
