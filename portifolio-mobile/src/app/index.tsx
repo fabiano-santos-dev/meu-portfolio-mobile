21d20ad26e5c1db7605c459e04e241018052ac41
Fabiano Alves Santos <fabiano.alves.dev@gmail.com>
2026-09-06 22:24:43 -0300

    feat(home): criada home

--- portifolio-mobile/src/app/index.tsx
+++ portifolio-mobile/src/app/index.tsx
@@ -1,6 +1,6 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { Image, Pressable, StyleSheet, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-
 import Cabecalho from '../componentes/Cabecalho';
 
 export default function HomeScreen() {
@@ -9,6 +9,7 @@ export default function HomeScreen() {
       <Cabecalho />
 
       <View style={styles.conteudo}>
+        <Image source={require('../../assets/fs1.png')} style={styles.foto} />
         <Text style={styles.introducao}>Olá, eu sou</Text>
 
         <Text style={styles.nome}>
@@ -22,6 +23,27 @@ export default function HomeScreen() {
           construindo minha experiência através de projetos modernos, funcionais
           e responsivos.
         </Text>
+        <View style={styles.botoes}>
+          <Pressable style={styles.botaoProjetos}>
+            <Text style={styles.textoBotaoProjetos}>Meus projetos →</Text>
+          </Pressable>
+          <Pressable style={styles.botaoCurriculo}>
+            <Text style={styles.textoBotaoCurriculo}>Ver currículo ↓</Text>
+          </Pressable>
+        </View>
+        <View style={styles.redesSociais}>
+          <Pressable>
+            <Ionicons name='logo-github' size={28} color='#ffffff' />
+          </Pressable>
+
+          <Pressable>
+            <Ionicons name='logo-linkedin' size={28} color='#ffffff' />
+          </Pressable>
+
+          <Pressable>
+            <Ionicons name='mail-outline' size={28} color='#ffffff' />
+          </Pressable>
+        </View>
       </View>
     </SafeAreaView>
   );
@@ -67,5 +89,49 @@ const styles = StyleSheet.create({
     color: '#dbeafe',
     fontSize: 16,
     lineHeight: 26,
+  },
+  foto: {
+    width: 230,
+    height: 230,
+    borderRadius: 10,
+    marginBottom: 30,
+    alignSelf: 'center',
+  },
+  botoes: {
+    flexDirection: 'row',
+    gap: 14,
+    marginTop: 30,
+  },
+
+  botaoProjetos: {
+    backgroundColor: '#3b82f6',
+    paddingVertical: 14,
+    paddingHorizontal: 20,
+    borderRadius: 7,
+  },
+
+  textoBotaoProjetos: {
+    color: '#ffffff',
+    fontSize: 15,
+    fontWeight: 'bold',
+  },
+
+  botaoCurriculo: {
+    borderWidth: 1,
+    borderColor: '#94a3b8',
+    paddingVertical: 14,
+    paddingHorizontal: 20,
+    borderRadius: 7,
+  },
+
+  textoBotaoCurriculo: {
+    color: '#ffffff',
+    fontSize: 15,
+    fontWeight: 'bold',
+  },
+  redesSociais: {
+    flexDirection: 'row',
+    gap: 22,
+    marginTop: 25,
   },
 });
